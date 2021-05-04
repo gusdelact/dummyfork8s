e6afc3ce726dcaff6dc4e88c2955b527819ed664
EC2 Default User <ec2-user@ip-10-100-0-190.ec2.internal>
2021-05-04 19:59:58 +0000

    a simple build.sh script

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-docker run -it --rm --name ejemplo02-build -v $PWD:/usr/src/app -w /usr/src/app maven:3.8-amazoncorretto-8 mvn clean install
+
+
+#builid jar file
+docker run -it --rm --name ejemplo02-build -v $PWD:/usr/src/app -w /usr/src/app \
+         	maven:3.8-amazoncorretto-8 \
+	       	mvn clean install
+#build container image
+docker build -t ejemplo02:latest -f  Dockerfile-spring .
+#and then push to your container image registry
